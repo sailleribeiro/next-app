b8162008b0bf5efe388b43df5a6542fd72c3f5d2
Ellias Ribeiro <elliasribeiro99@gmail.com>
2024-10-25 17:04:15 -0300

    feat: add lucide icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,14 @@ export default function Home() {
   const { router } = useUtils();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-4 row-start-2 items-end">
-        <div className="flex items-start justify-start w-full">
-          <h1 className="text-3xl font-bold">Login</h1>
-        </div>
+      <main className="flex flex-col gap-4 row-start-2">
+        <h1 className="text-3xl font-bold">Login</h1>
         <Input placeholder="Email" type="email" />
         <Input placeholder="Senha" type="password" />
-        <Button onClick={() => router.push("/pages/dashboard")}>
+        <Button
+          className="w-full"
+          onClick={() => router.push("/pages/dashboard")}
+        >
           <ArrowTopRightIcon />
           Entrar
         </Button>
